7565accddc8a880707ca3de5c9e4fe246536f1f6
MIkhail <jazzskill2@gmail.com>
2018-03-19 22:11:43 +0300

    update some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 # There is like interface for getting inverse of matrix and the origin matrix
 makeCacheMatrix <- function(matrixForCache = matrix()) {
   inv <- NULL
+# we have to drop inverse after set new matrix
   set <- function(newMatrix) {
     matrixForCache <<- newMatrix
     inv <<- NULL
@@ -8,6 +9,7 @@ makeCacheMatrix <- function(matrixForCache = matrix()) {
   get <- function() matrixForCache
   setInverse <- function(inverse) inv <<- inverse
   getInverse <- function() inv
+  
   list(set = set,
        get = get,
        setInverse = setInverse,
@@ -17,12 +19,15 @@ makeCacheMatrix <- function(matrixForCache = matrix()) {
 # This is a function that uses for caching inverse, it works only with result of makeCacheMatrix(yourMatrix)
 cacheSolve <- function(matrixObject, ...) {
   matrixInverse <- matrixObject$getInverse()
+  
   if(!is.null(matrixInverse)) {
     message("getting cached data")
     return(matrixInverse)
   }
+  
   matr <- matrixObject$get()
   matrixInverse <- solve(matr, ...)
   matrixObject$setInverse(matrixInverse)
+  
   matrixInverse
 }
